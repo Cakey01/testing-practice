889136cab15402764854ccfb18829499acca82c0
Cakey01 <alexanderwarren7@gmail.com>
2026-06-16 17:09:24 -0400

    Create and test capitalize function

--- index.test.js
+++ index.test.js
@@ -1,5 +1,20 @@
 import capitalize from './capitalize.js';
 
+// capitalize tests
+
 test('returns string', () => {
-  expect(capitalize('string')).toBe('string');
+  expect(capitalize('string')).toBeDefined();
 });
+
+test('returns String', () => {
+  expect(capitalize('string')).toBe('String');
+});
+
+test('returns Word', () => {
+  expect(capitalize('word')).toBe('Word');
+});
+
+test('returns already capitalized word', () => {
+  expect(capitalize('Word')).toBe('Word');
+});
+
